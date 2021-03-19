cc5fea72c967f124866a4e7ae55d299ee0192342
Sloan Quinn <sloan.quinn@gmail.com>
2021-03-19 07:35:47 -0500

    [WIP] Feat: Messaging system
    - Implemented specialty Message types: DataResponse, ViewRequest
    - Added type field to Message interface for Message routing
    - Added basic Message routing to CoreBroker
    - Added (but did not yet implement) Observer to Broker interface

--- src/Messaging/CoreBroker.ts
+++ src/Messaging/CoreBroker.ts
@@ -1,15 +1,23 @@
 import { DAO, IController, SecurityManager } from "../Core";
 
-import { Broker, Message, BrokerMessage } from "./";
-import { Subject } from "rxjs";
+import { Broker, Message} from "./";
+import { Observer, Subject } from "rxjs";
 import { UIDriver } from "./UIDriver";
+
+export enum SystemComponents {
+  Controller = "CONTROLLER",
+  Security = "SECURITY",
+  DAO = "DAO",
+  UI = "UI",
+  Module = "MODULE",
+}
 
 export class CoreBroker implements Broker {
   private static instance: CoreBroker;
-  private controller: Subject<Message>;
-  private dataAccess: Subject<Message>;
-  private security: Subject<Message>;
-  private viewDriver: Subject<Message>;
+  private readonly controller: Subject<Message>;
+  private readonly dataAccess: Subject<Message>;
+  private readonly security: Subject<Message>;
+  private readonly viewDriver: Subject<Message>;
 
   private constructor(core: IController) {
     this.controller = new Subject<Message>();
@@ -33,7 +41,65 @@ export class CoreBroker implements Broker {
     return this.instance;
   }
 
-  publish(msg: BrokerMessage): void {}
+  publish(msg: Message): void {
+    const route = this.getRoute(msg);
+
+    if (route !== this) {
+      route.next(msg);
+    }
+  }
 
   subscribe(): void {}
+
+  private getRoute(msg: Message): Observer<Message> {
+    let route: Observer<Message> = this;
+
+    // First, route based on Message type
+    switch (msg.type) {
+      case "Security":
+        route = this.security;
+        break;
+      case "DataRequest":
+        route = this.dataAccess;
+        break;
+      case "ViewRequest":
+        route = this.controller;
+        break;
+      case "DataResponse":
+      case "Transient":
+        // Second, route based on destination field
+        if (msg.to in SystemComponents) {
+          switch (msg.to) {
+            case SystemComponents.DAO:
+              route = this.dataAccess;
+              break;
+            case SystemComponents.Controller:
+              route = this.controller;
+              break;
+            case SystemComponents.Security:
+              route = this.security;
+              break;
+            case SystemComponents.UI:
+              route = this.viewDriver;
+              break;
+            case SystemComponents.Module:
+              // TODO Work out how this needs to work -
+              //  Module in .to indicates the Message needs to be
+              //  handed off to the other broker
+              throw new Error("Cannot find route");
+          }
+        }
+        break;
+      default:
+        throw new Error("Cannot find route");
+    }
+
+    return route;
+  }
+
+  complete(): void {}
+
+  error(err: any): void {}
+
+  next(value: Message): void {}
 }
--- src/Messaging/Message.ts
+++ src/Messaging/Message.ts
@@ -1,33 +1,45 @@
-import { IController } from "../Core";
-import { AppModule } from "../Modules";
+import { View } from "../Modules";
+import { SystemComponents } from "./CoreBroker";
 
 /**
  * The base message type.
  */
 export interface Message {
+  type: String;
   // TODO: implement SystemComponent interface for Message routing purposes
-  from: unknown;
-  to: unknown;
+  from: SystemComponents;
+  to: SystemComponents;
+  received: Boolean;
   request: Message | Function | String;
   response?: Promise<unknown>;
-}
-
-export interface BrokerMessage extends Message {
-  from: IController | AppModule;
-  to: "controller" | "module";
 }
 
 /**
  * A Message type for requests for data from the database.
  * Broker will prioritize routing first through auth check, then to DAO.
  */
-export interface DataRequest extends Message {}
+export interface DataRequest extends Message {
+  type: "DataRequest";
+  to: SystemComponents.DAO;
+}
+
+/**
+ * A Message type for responding to Data Requests.
+ * This type flips the "to" and "from" fields from the original request,
+ * includes the original request Message under "request", and the
+ * retrieved data, as an array of Records, inside a Promise in the "response" field.
+ */
+export interface DataResponse extends Message {
+  type: "DataResponse";
+  Response: Promise<Array<Record<string, unknown>>>;
+}
 
 /**
  * A Message type for requests for security functions - auth, etc.
  * Broker will prioritize routing directly to Security system.
  */
 export interface SecurityMessage extends Message {
+  type: "Security";
   // the requested Security operation
   operation: "authenticate" | "authorize";
 }
@@ -37,6 +49,19 @@ export interface SecurityMessage extends Message {
  * Used for hand-offs between Brokers.
  */
 export interface TransientMessage extends Message {
+  type: "Transient";
   // request type for this type of Message is another Message, to be sent to the final recipient
   request: Message;
+}
+
+/**
+ * A Message type for passing Views between the UI, Controller, and Modules.
+ * This Message type takes a String - the name of the particular View being requested -
+ * as the "request" field.
+ */
+export interface ViewMessage extends Message {
+  type: "View";
+  request: String;
+  // response type for this Message type is a Promise that resolves into a View
+  response: Promise<View>;
 }
