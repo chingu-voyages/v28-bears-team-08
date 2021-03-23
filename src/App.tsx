39f92ee4830190af49fec5dabcce231735e7cf37
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-22 21:14:21 -0400

    apply component added with placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import theme from './Theme/theme';
 import Login from './Components/login.component';
 import Register from './Components/register.component';
 import Profile from './Components/profile.component';
+import Apply from './Components/apply.component';
 
 const App: FunctionComponent = () => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
@@ -28,11 +29,13 @@ const App: FunctionComponent = () => {
             render={(props) => <Register {...props} handleAuth={handleAuth} />}
           />
           <Route exact path="/profile" component={Profile} />
+          <Route exact path="/apply" component={Apply} />
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
   );
 };
+//TODO: check component name with Cesar.
 
 export default App;
 
---
+++ src/Components/apply.component.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent, useState, useEffect } from 'react';
+export interface ApplyProps {
+  appStatus: String;
+  dateOfBirth: Date;
+  tin: String;
+  gender: String;
+  race: String;
+  ethnicity: String;
+  education: String;
+  // income: new Schema({
+  //   has: Boolean,
+  //   monthly: Number,
+  // })
+}
+//Q: How to handle nesting in interface?
+
+const Apply: FunctionComponent<ApplyProps> = () => {
+  const [app, setApp] = useState<ApplyProps | null>(null);
+  //app properties as pieces of state to come
+  return (
+    <h3>
+      This is Apply component placeholder. Need to final schema to write form
+    </h3>
+    //application form controlled component to come
+  );
+};
+
+export default Apply;
