46e83d41b65bef4f39866fdcf5628a97fa81a809
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-23 19:50:00 -0400

    add small comment

--- src/Components/appstatus.component.tsx
+++ src/Components/appstatus.component.tsx
@@ -9,6 +9,7 @@ const AppStatus: FunctionComponent<AppStatusProps> = (props) => {
     <h3>Progress chart to COME</h3>
     // https://css-tricks.com/html5-progress-element/
     //progress chart with current status highlighted in whole flowchart
+    //write a function that converts appStatus to a bracket in the progress bar
   );
 };
 
