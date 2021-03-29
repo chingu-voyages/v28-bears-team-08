4f81773ba38359dbf2828e66df6d36ba35722ab2
Joohoon Kim <jayjhkim7@gmail.com>
2021-03-28 22:15:48 -0400

    employeeregister placeholder

---
+++ src/Components/employeeRegister.component.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from 'react';
+
+//Employee needs only to provide company email and password to register. Other information is seeded into db at hiring.
+const EmployeeRegister: FunctionComponent = () => {
+  return <h3>Seed db with other info, only password </h3>;
+};
+
+export default EmployeeRegister;
